f6366450b934b2af8929e8d031726325e2c0f52e
shaamomer <ahdahir02@gmail.com>
2026-06-07 18:58:18 +0300

    exerise 1

---
+++ exercise #1/exercise1.js
@@ -0,0 +1,17 @@
+let x = 7;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+console.log(x++);
+console.log("post increament");
+console.log(x);
+
+console.log(++y);
+console.log("pre increament");
+console.log(y);
+
